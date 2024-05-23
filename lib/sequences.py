ab8262b3cd334f79a576e26d2858e9638e8d1ce4
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-23 23:51:40 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n < 1:
+        print([])
+        return
+    
+    fibonacci_sequence = [0]
+    
+    if n > 1:
+        fibonacci_sequence.append(1)
+    
+    while len(fibonacci_sequence) < n:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
